a579006a424c019ed171d8dfb2ca7123813bb8ba
Nate <supermanheng21@gmail.com>
2018-05-05 15:49:00 +0800

    webpage

---
+++ src/main/java/online/abot/alertbot/controller/HomeController.java
@@ -0,0 +1,15 @@
+package online.abot.alertbot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
